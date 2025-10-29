31d91bfa962f09527139b788dc311d7c9b44cf1a
Mukesh Kanna <MUKESHKANNA85@GMAIL.COM>
2025-10-29 19:06:22 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ window.addEventListener('load', function(){
 // iTyped 
 
 window.ityped.init(document.querySelector('.iTyped'), {
-    strings: ["I'm a Web Developer", 'I Love Html5', 'I Love CSS3', 'I Love Jquery', 'I Love Laravel'],
+    strings: ["IT Professional", 'Graphics Technology', 'Photography', 'Networking', 'Hardware','Traveller'],
     loop: true
 });
 
